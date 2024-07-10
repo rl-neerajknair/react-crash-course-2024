b4309e9b320ef759a62c881698342cccf89d3781
RunelightRL <87036518+RunelightRL@users.noreply.github.com>
2024-07-10 16:09:27 +0530

    Delete Job function successfully impleted using DELETE method.

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 // import { useState, useEffect } from 'react'
-import { useParams, useLoaderData } from 'react-router-dom';
+import { useParams, useLoaderData, useNavigate } from 'react-router-dom';
 import { FaArrowLeft, FaMapMarker } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-const JobPage = () => {
+import { toast } from 'react-toastify';
+
+const JobPage = ({ deleteJob }) => {
+    const navigate = useNavigate();
     const { id } = useParams();
     const job = useLoaderData();
+
+    const onDeleteClick = (jobId) => {
+        const confirm = window.confirm('Are you sure you want to delete this listing?')
+
+        if (!confirm) return;
+
+        deleteJob(jobId);
+
+        toast.success('Job Deleted Successfully');
+
+        navigate('/jobs');
+    }
 
     // const [job, setJob] = useState(null);
     // const [loading, setLoading] = useState(true)
@@ -103,7 +118,7 @@ const JobPage = () => {
                                     to={`/jobs/edit/${job.id}`}
                                     className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                                 >Edit Job</Link>
-                                <button
+                                <button onClick={() => onDeleteClick(job.id)}
                                     className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                                 >
                                     Delete Job
